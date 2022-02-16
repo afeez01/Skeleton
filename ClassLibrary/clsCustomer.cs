bb1bfa8dcf317a2cb5602c33f50e9aaa9b0a8e72
afeez01 <ardz.afeez@gmail.com>
2022-02-16 18:09:19 +0000

    added further customer class test

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public short CustomerID { get; set; }
+        public DateTime DOB { get; set; }
+        public string EmailAddress { get; set; }
+        public string CustomerDetails { get; set; }
+        public decimal AccountBalance { get; set; }
+        public bool OrderProcess { get; set; }
     }
 }
--- Testing2/tstCustomer.cs
+++ Testing2/tstCustomer.cs
@@ -14,5 +14,66 @@ namespace Testing2
             
             Assert.IsNotNull(AnCustomer);
         }
+
+
+        [TestMethod]
+        public void CustomerIdOK()
+
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Int16 TestData = 1;
+            AnCustomer.CustomerID = TestData;
+            Assert.AreEqual(AnCustomer.CustomerID, TestData);
+        }
+
+        [TestMethod]
+        public void DateOfBirthOK()
+
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            DateTime TestData = DateTime.Now.Date;
+            AnCustomer.DOB = TestData;
+            Assert.AreEqual(AnCustomer.DOB, TestData);
+        }
+
+        [TestMethod]
+        public void EmailAddressOK()
+
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestData = "toolsunited@gmail.com";
+            AnCustomer.EmailAddress = TestData;
+            Assert.AreEqual(AnCustomer.EmailAddress, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerDetailsOK()
+
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestData = "bob marley";
+            AnCustomer.CustomerDetails = TestData;
+            Assert.AreEqual(AnCustomer.CustomerDetails, TestData);
+        }
+
+        [TestMethod]
+        public void AccountBalanceOK()
+
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            decimal TestData = 1;
+            AnCustomer.AccountBalance = TestData;
+            Assert.AreEqual(AnCustomer.AccountBalance, TestData);
+        }
+
+        [TestMethod]
+        public void OrderProcessOK()
+
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Boolean TestData = true;
+            AnCustomer.OrderProcess = TestData;
+            Assert.AreEqual(AnCustomer.OrderProcess, TestData);
+        }
     }
 }
