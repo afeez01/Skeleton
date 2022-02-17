3b8f1ffb001225d56ee47910812a969f0cfaffc4
P2622409 <p2622409@my365.dmu.ac.uk>
2022-02-17 22:28:16 +0000

    Creating the properties for Stock/Tool table design(W20)

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public int ToolID { get; set; }
+        public string ToolName { get; set; }
+        public int QuantityInStock { get; set; }
+        public DateTime DateAdded { get; set; }
+        public decimal UnitPrice { get; set; }
+        public bool OnSale { get; set; }
     }
 }
