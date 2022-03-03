5313334a53bc389ee10084fea563d315c4e03642
afeez01 <ardz.afeez@gmail.com>
2022-03-03 19:20:41 +0000

    finished week 21

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create new instance of class
+        clsCustomer AnCustomer = new clsCustomer();
+        //get data from session object
+        AnCustomer = (clsCustomer)Session["AnCustomer"];
+        //display email address on the page
+        Response.Write("ID =" + AnCustomer.CustomerID + "<br />");
+        Response.Write("Date Of Birth =" + AnCustomer.DOB + "<br />");
+        Response.Write("Email =" + AnCustomer.EmailAddress + "<br />");
+        Response.Write("Details =" + AnCustomer.CustomerDetails + "<br />");
+        Response.Write("Balance =" + AnCustomer.AccountBalance + "<br />");
+        Response.Write("Order Process =" + AnCustomer.OrderProcess + "<br />");
+
+
+
+
 
     }
 }
