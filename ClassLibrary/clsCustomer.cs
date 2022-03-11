c660003f57b4ae1ab7bbadb28b16fd72d6355f46
afeez01 <ardz.afeez@gmail.com>
2022-03-11 18:49:39 +0000

    added cls data connection

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -124,14 +124,31 @@ namespace ClassLibrary
 
         public bool Find(int CustomerID)
         {
-            mCustomerID = 21;
-            mDOB = Convert.ToDateTime("16/9/2016");
-            mEmailAddress = "testemail@customer.com";
-            mCustomerDetails = "Test Name";
-            mAccountBalance = 41;
-            mOrderProcess = true;
-            //always return true
-            return true;
-        }
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            // add the parameter for the address no to search for
+            DB.AddParameter("@CustomerID", CustomerID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblCustomer_filterByCustomerID");
+            //if one record is found (there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mCustomerID = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerID"]);
+                mEmailAddress = Convert.ToString(DB.DataTable.Rows[0]["EmailAddress"]);
+                mCustomerDetails = Convert.ToString(DB.DataTable.Rows[0]["CustomerDetails"]);
+                mAccountBalance = Convert.ToDecimal(DB.DataTable.Rows[0]["AccountBalance"]);
+                mDOB = Convert.ToDateTime(DB.DataTable.Rows[0]["DOB"]);
+                mOrderProcess = Convert.ToBoolean(DB.DataTable.Rows[0]["OrderProcess"]);
+                //return that everything worked OK
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating a problem
+                return false;
+
+            }            }
     }
 }
