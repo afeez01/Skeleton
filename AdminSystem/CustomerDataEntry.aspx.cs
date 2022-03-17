a0fdd218cd173d3a5b88ad5a4b38e0fa450f8152
afeez01 <P2537202@my365.dmu.ac.uk>
2022-03-17 06:42:49 +0000

    adding validation to presentation layer

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -16,21 +16,36 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
+
+
         //create new instance of clsAddress
         clsCustomer AnCustomer = new clsCustomer();
-        //capture email address
-        AnCustomer.CustomerID = Convert.ToInt16(txtCustomerID.Text);
-        AnCustomer.DateOfBirth = Convert.ToDateTime(txtDateOfBirth.Text);
-        AnCustomer.EmailAddress = txtEmailAddress.Text;
-        AnCustomer.CustomerDetails = txtCustomerDetails.Text;
-        AnCustomer.AccountBalance = Convert.ToDecimal(txtAccountBalance.Text);
-        AnCustomer.OrderProcess = Convert.ToBoolean(chkOrderProcess.Checked);
-        //store the date of birth in the session object
-        Session["AnCustomer"] = AnCustomer;
-        //navigate to viewer page
-        Response.Redirect("CustomerViewer.aspx");
+        string EmailAddress = txtEmailAddress.Text;
+        string DateOfBirth = txtDateOfBirth.Text;
+        string CustomerDetails = txtCustomerDetails.Text;
+        string AccountBalance = txtAccountBalance.Text;
+        string Error = "";
+        Error = AnCustomer.Valid(DateOfBirth, EmailAddress, CustomerDetails, AccountBalance);
+        if (Error == "")
+        {
 
 
+            //capture email address
+            AnCustomer.CustomerID = Convert.ToInt16(txtCustomerID.Text);
+            AnCustomer.DateOfBirth = Convert.ToDateTime(txtDateOfBirth.Text);
+            AnCustomer.EmailAddress = txtEmailAddress.Text;
+            AnCustomer.CustomerDetails = txtCustomerDetails.Text;
+            AnCustomer.AccountBalance = Convert.ToDecimal(txtAccountBalance.Text);
+            AnCustomer.OrderProcess = Convert.ToBoolean(chkOrderProcess.Checked);
+            //store the date of birth in the session object
+            Session["AnCustomer"] = AnCustomer;
+            //navigate to viewer page
+            Response.Redirect("CustomerViewer.aspx");
+        }
+        else
+        {
+            lblError.Text = Error;
+        }
     }
 
 
