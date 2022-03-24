e95b293121bd186105c091f4615cfafc264e6b11
P2622409 <p2622409@my365.dmu.ac.uk>
2022-03-24 16:37:45 +0000

    Creating the Collection Class (Properties) - part 2

--- ClassLibrary/clsStockCollection.cs
+++ ClassLibrary/clsStockCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace ClassLibrary
 {
@@ -6,6 +7,68 @@ namespace ClassLibrary
     {
         // private data member for the list
         List<clsStock> mStockList = new List<clsStock>();
+
+        public clsStockCollection()
+        {
+            // var for the index
+            Int32 Index = 0;
+            // var to store the record count
+            Int32 RecordCount = 0;
+            // object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            // execute the stored procedure
+            DB.Execute("sproc_tblTool_SelectAll");
+            // get count of records
+            RecordCount = DB.Count;
+
+            // while there are records to process do
+            while (Index < RecordCount)
+            {
+                //create instance of Sock class
+                clsStock AnStock = new clsStock();
+
+                // read in the fields from the current record
+                AnStock.ToolID = Convert.ToInt32(DB.DataTable.Rows[Index]["ToolID"]);
+                AnStock.ToolName = Convert.ToString(DB.DataTable.Rows[Index]["ToolName"]);
+                AnStock.QuantityInStock = Convert.ToInt32(DB.DataTable.Rows[Index]["QuantityInStock"]);
+                AnStock.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+                AnStock.UnitPrice = Convert.ToDecimal(DB.DataTable.Rows[Index]["UnitPrice"]);
+                AnStock.OnSale = Convert.ToBoolean(DB.DataTable.Rows[Index]["OnSale"]);
+
+                // add the record to the private data member
+                mStockList.Add(AnStock);
+
+                // point at the next rexord
+                Index++;
+            }
+
+            /*//create the item of test data
+            clsStock TestItem = new clsStock();
+
+            // set its properties
+            TestItem.ToolID = 1;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.OnSale = false;
+            TestItem.QuantityInStock = 7;
+            TestItem.ToolName = "Bosch HSS-R Drill Bit";
+            TestItem.UnitPrice = 1.9500m;
+            // add item to the test list
+            mStockList.Add(TestItem);
+
+            // reinitialise the object for some new data
+            TestItem = new clsStock();
+
+            // set its properties
+            TestItem.ToolID = 2;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.OnSale = true;
+            TestItem.QuantityInStock = 2;
+            TestItem.ToolName = "Bahco 244 Super Sharp Hard Point Hand Saw";
+            TestItem.UnitPrice = 6.9500m;
+            // add item to the test list
+            mStockList.Add(TestItem);*/
+
+        }
 
         public List<clsStock> StockList 
         { 
