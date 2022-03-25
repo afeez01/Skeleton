9b5de34c6f47867a9fd3216a05ba939fd632f9e6
Duarte Gama Santos <P2607026@my365.dmu.ac.uk>
2022-03-25 22:01:12 +0000

    qeqwe

--- AdminSystem/SuppliersDataEntry.aspx.cs
+++ AdminSystem/SuppliersDataEntry.aspx.cs
@@ -8,19 +8,26 @@ using System.Web.UI.WebControls;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
+    protected void cbGlobal_CheckedChanged(object sender, EventArgs e)
+    {
+        bool GlobalSupplier = true;
+    }
 
     protected void btnRegister_Click(object sender, EventArgs e)
     {
+        clsSupplier ASupplier = new clsSupplier();
+
+        ASupplier.Address = txtAddress.Text;
+        ASupplier.DateAdded = DateTime.Today;
+        ASupplier.GlobalSupplier = cbGlobal.Checked;
+
+        Session["ASupplier"] = ASupplier;
+
         Response.Redirect("SuppliersViewer.aspx");
     }
 
     protected void btnCancel_Click(object sender, EventArgs e)
     {
-
-    }
-
-    protected void cbGlobal_CheckedChanged(object sender, EventArgs e)
-    {
-
+        Response.Redirect("SuppliersDataEntry.aspx");
     }
 }
